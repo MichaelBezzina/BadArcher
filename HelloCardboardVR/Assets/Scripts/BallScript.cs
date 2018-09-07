edfc3f1ecdc483c9182b69eaf188630bf7640936
MichaelBezzina <michaelbezzina@outlook.com>
2018-09-07 15:55:23 -0600

    Project Import

---
+++ HelloCardboardVR/Assets/Scripts/BallScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BallScript : MonoBehaviour {
+    public ParticleSystem explosion;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        explosion.transform.position = this.transform.position; //You can add this so that the effect appears where the collision happens. In case there is more than one collision
+        Debug.Log(explosion.transform.position);
+        explosion.Play();
+        new WaitForSeconds(.5f);
+        Destroy(gameObject);
+    }
+}
---
+++ HelloCardboardVR/Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour {
+
+    public BallScript ballPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0) || OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.LTouch) > 0.5 || OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger, OVRInput.Controller.RTouch) > 0.5)
+        {
+            BallScript ball = Instantiate<BallScript>(ballPrefab);
+            ball.transform.localPosition = transform.position;
+
+            ball.GetComponent<Rigidbody>().AddForce(Camera.main.transform.forward * Random.Range(500, 750));
+        }
+    }
+}
